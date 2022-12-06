fabf2113bf3d34f2439cc99ddc98c0aedfa85910
Gabriel Alves <113126048+alvesgabrieel@users.noreply.github.com>
2022-12-06 20:47:31 -0300

    add arquivo_correto novas_funcionalidades

---
+++ exercicio_modulo7/main.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const numeroA = parseInt(document.getElementById('campoA').value);
+    const numeroB = parseInt(document.getElementById('campoB').value);
+    const msgSucesso = 'Valores enviados com <b>sucesso<b/>!';
+    const msgErro = 'Desculpe, o segundo valor <em>(Campo B)</em> precisa ser <b>maior</b> que o primeiro valor <em>(Campo A)</em>'
+
+    if(numeroA < numeroB) {
+
+        let containerMsg = document.querySelector('.msg-retorno');
+        containerMsg.innerHTML = msgSucesso;
+        containerMsg.style.display = 'block';
+        containerMsg.style.background = 'green';
+
+    } else {
+        let containerMsg = document.querySelector('.msg-retorno');
+        containerMsg.innerHTML = msgErro;
+        containerMsg.style.display = 'block';
+        containerMsg.style.background = 'red';
+    }
+    var numeroA1 = document.getElementById('campoA');
+    numeroA1.value = '';
+    var numeroB1 = document.getElementById('campoB');
+    numeroB1.value = '';
+})
+
+    
+
+        
