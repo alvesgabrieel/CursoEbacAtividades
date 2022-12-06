e02bb2fed9f89adcbbb534aff5cab4653e86b6db
Gabriel Alves <113126048+alvesgabrieel@users.noreply.github.com>
2022-12-06 20:41:10 -0300

    update novas_funcionalidades main.js

--- exercicio_modulo7/main.js
+++ exercicio_modulo7/main.js
@@ -1,22 +1,54 @@
-const form = document.getElementById('form');
+const form = document.getElementById('forms');
+const contatosNome = [];
+const contatosNumeros = [];
+
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const numeroA = parseInt(document.getElementById('campoA').value);
-    const numeroB = parseInt(document.getElementById('campoB').value);
-    const msgSucesso = 'Valores enviados com <b>sucesso<b/>!';
-    const msgErro = 'Desculpe, o segundo valor <em>(Campo B)</em> precisa ser <b>maior</b> que o primeiro valor <em>(Campo A)</em>'
-
-    if(numeroA < numeroB) {
-
-        let containerMsg = document.querySelector('.msg-retorno');
-        containerMsg.innerHTML = msgSucesso;
-        containerMsg.style.display = 'block';
-
-    } else {
-        let containerMsg = document.querySelector('.msg-retorno');
-        containerMsg.innerHTML = msgErro;
-        containerMsg.style.display = 'block';
-    }
+    adicionarContatos();
+    atualizaContatos();
 })
+
+function adicionarContatos () {
+
+    let nome = document.getElementById('nome');
+    let numero = document.getElementById('numero');
+    let valorNome = nome.value;
+    let valorNumero = numero.value;
+
+    valorNome.split(' ');
+
+    if(valorNome.split(' ').length < 2){
+        alert('Adicione um sobrenome')
+    } else {
+        if(valorNumero.length < 10){
+            alert('Adicione um número válido')
+        }  else {
+            
+            if(contatosNumeros.includes(valorNumero)){
+                alert(`numero ${valorNumero} já adicionado aos contatos`);
+            } else {
+    
+                contatosNome.push(valorNome);
+                contatosNumeros.push(valorNumero)
+        
+                let linha = `<tr>`;
+                linha += `<td>${valorNome}</td>`;
+                linha += `<td>${valorNumero}</td>`;
+                linha += `</tr>`;
+                linhas += linha;
+        
+                alert(`contato ${valorNome} adicionado na agenda`);   
+            }
+        } 
+    }
+            nome.value = '';
+            numero.value = '';
+}
+
+function atualizaContatos(){
+    const contato = document.querySelector('tbody');
+    contato.innerHTML = linhas;
+}
