0c0de64399eb09f858d9a74345c093bc0f389f59
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-02-28 18:15:47 -0300

    initial commit

---
+++ exercicio_ts/src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// ==> Função UM
+const multiplicar = (numeroUm, numeroDois) => {
+    return numeroUm * numeroDois;
+};
+console.log(multiplicar(5, 10));
+// ==> Função DOIS
+const saudacao = (nome) => {
+    console.log(`Olá ${nome}`);
+};
+saudacao('Gabriel');
---
+++ exercicio_ts/src/main.ts
@@ -0,0 +1,14 @@
+// ==> Função UM
+const multiplicar = (numeroUm: number, numeroDois: number): any => {
+    return numeroUm * numeroDois;
+}
+
+console.log(multiplicar(5, 10));
+
+// ==> Função DOIS
+const saudacao = (nome: string) => {
+    console.log(`Olá ${nome}`) ;
+}
+
+saudacao('Gabriel');
+
